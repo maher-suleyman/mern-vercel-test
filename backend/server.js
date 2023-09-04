5531dfc817dcd5e53e2e37fd5ff169724c23359d
Maher Suleyman <maher.suleyman.ms@gmail.com>
2023-09-04 11:53:57 -0400

    Fix for vercel

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/data", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ message: "Hello from the backend!" });
 });
 
